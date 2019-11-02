3844944c66ed17bfbfd9a2e79d0d4fbe40a3823d
verje <jesus_jarm@yahoo.com>
2019-11-02 18:19:35 -0500

    upload after remove dbname in the uri connection string

--- database/conexion.js
+++ database/conexion.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var keys = require('../config/keys');
 
 db = mongoose.createConnection(keys.mongodb.dbURI, {
-  dbName: 'test',
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
